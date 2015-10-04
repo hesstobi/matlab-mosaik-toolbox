94efe03755462dc3cb8f6b254744e3e7c3584123
Sasan Jacob Rasti <sj.rasti@gmail.com>
2015-10-04 15:51:41 +0200

    Changed model Load to work with ModelSimulator

--- Example/Battery-Load-Simulation/Load.m
+++ Example/Battery-Load-Simulation/Load.m
@@ -1,5 +1,5 @@
-classdef Load < MosaikAPI.Simulator
-	%LOAD electrical load simulator for MOSAIK
+classdef Load < MosaikAPI.Model
+	% LOAD electrical load model for MOSAIK ModelSimulator
 	%   Simulates a load with given parameters resistance R, operating voltage U_R and tolerance delta, which together
 	%   define the range operating voltages.
 	%   Based on the battery given voltage U, resistance R and time (step.size) t , the load consumes capacitance Q in the form
@@ -9,141 +9,66 @@ classdef Load < MosaikAPI.Simulator
 	%   and feeds it the consumed capacitance Q via set_data.
 
 	properties
-		sid;
-		step_size;
-		value;
-		loads = struct;			
-		amount = 0;
+		resistance;
+		voltage;
+		tolerance;
+		voltage_in;
+		consumed_capacitance;
+		data_out;
 	end
 
 	methods 
 
-		function sim = Load(server)
-			load_meta = struct( ...
-			'models', struct( ...				
-				'Load', struct( ...
-					'public', true, 'params', {{'resistance', 'voltage', 'tolerance'}}, 'attrs', {{'voltage_in', 'data_out'}} ...
-					) ...
-				) ...
-			);
-			sim = sim@MosaikAPI.Simulator(server, load_meta);
+		function this = Load(sim,eid,varargin)
+			this = this@MosaikAPI.Model(sim,eid);
+            
+            p = inputParser;
+            addOptional(p,'resistance',1000,@(x)validateattributes(x,{'numeric'},{'scalar'}));
+            addOptional(p,'voltage',10,@(x)validateattributes(x,{'numeric'},{'scalar'}));
+            addOptional(p,'tolerance',0.1,@(x)validateattributes(x,{'numeric'},{'scalar'}));
+            parse(p,varargin{:});
+            
+            this.resistance = p.Results.resistance;
+            this.voltage = p.Results.voltage;
+            this.tolerance = p.Results.tolerance;
+            this.consumed_capacitance  = 0;
+		end
+
+		function step(this,varargin)
+			p = inputParser;
+			addOptional(p,'voltage_in',struct); %Add validation (only one voltage_in allowed)
+			parse(p,varargin{:});
+
+			voltage_in = cell2mat(struct2cell(p.voltage_in));
+			if ge(voltage_in,(this.voltage*(1-this.tolerance))) && le (voltage_in,(this.voltage*(1+this.tolerance)))
+				this.consumed_capacitance = ((voltage_in/this.resistance)*sim.step_size;
+			end
+
+			rels = sim.as_get_related_entities(this.eid);
+			fn_src_full_id = fieldnames(rels);
+			l = struct;
+			for j = 1:numel(fn_src_full_id)
+				if strcmp(rels.(fn_src_full_id{j}).type, 'Battery')
+					l.(fn_src_full_id{j}) = struct('consumed_capacitance', this.consumed_capacitance);
+				end			
+			end
+			m = struct;
+			m.([strrep(sim.sid, '-', '_0x2D_'), '_0x2E_', this.eid]) = l; % '_0x2D_' is hex for '-'; '_0x2E_' is hex for '.'; JSONLab will convert it, MATLab can not have points in struct fields.
+			sim.as_set_data(m);
+
+			this.data_out.consumed_capacitance = this.consumed_capacitance;
 		end
 
 	end
 
-	methods
+	methods (Static)
 
-		function meta = init(sim, args, kwargs)
-			sid = args{1};
-			if isfield(kwargs, 'step_size')
-				step_size = kwargs.step_size;
-			else
-				step_size = 10;
-			end
-
-			sim.sid = sid;
-			sim.step_size = step_size;
-
-			meta = sim.meta;			
-			             
-			
+		function value = meta()
+			value.public = true;
+			value.attrs = {'voltage_in','data_out'};
+			value.params = {'resistance','voltage','tolerance'};
+			value.any_inputs = false;
 		end
 
-		function entity_list = create(sim, args, kwargs)			
-			num = args{1};
-			model = args{2};
-			if isfield(kwargs, 'resistance')
-				resistance = kwargs.resistance;
-			else
-				resistance = 1000;
-			end
-			if isfield(kwargs, 'voltage')
-				voltage = kwargs.voltage;
-			else
-				voltage = 10;
-			end
-			if isfield(kwargs, 'tolerance')
-				tolerance = kwargs.tolerance;
-			else
-				tolerance = 1000;
-			end
-
-			entity_list = cell.empty;
-			for i = 1:num
-				l.('eid') = strcat('l', num2str(sim.amount));
-				l.('type') = model;
-				l.('rel') = {{}};
-				entity_list(end+1) = {l};
-				sim.loads.(l.('eid')).('resistance') = resistance;
-				sim.loads.(l.('eid')).('voltage') = voltage;
-				sim.loads.(l.('eid')).('tolerance') = tolerance;
-				sim.loads.(l.('eid')).('consumed_capacitance') = 0;
-				sim.amount = sim.amount + 1;
-			end
-
-			% Adds empty cell for JSONlab
-			if eq(numel(entity_list), 1)
-				entity_list(end+1) = {[]};
-			end
-		end
-
-		function time_next_step = step(sim, args, ~)
-			if iscell(args)
-				time = args{1};
-				inputs = args{2};
-			else
-				time = args;
-				inputs = struct;
-			end
-
-			fn_eid = fieldnames(inputs);
-			for i = 1:numel(fn_eid)
-				voltage_in = inputs.(fn_eid{i}).voltage_in;
-				fn_src_full_id = fieldnames(voltage_in);
-				if eq(numel(fn_src_full_id), 1)
-					voltage_in= inputs.(fn_eid{i}).voltage_in.(fn_src_full_id{1});
-				else
-					error('A load can only be connected to one battery.')
-				end
-				voltage = sim.loads.(fn_eid{i}).voltage;
-				tolerance = sim.loads.(fn_eid{i}).tolerance;
-				resistance = sim.loads.(fn_eid{i}).resistance;
-				if ge(voltage_in, (voltage * (1 - tolerance))) && le(voltage_in, (voltage * (1 + tolerance)))
-					consumed_capacitance = ((voltage_in / resistance) * sim.step_size);
-				else
-					consumed_capacitance = 0;
-				end
-
-				sim.loads.(fn_eid{i}).('consumed_capacitance') = sim.loads.(fn_eid{i}).('consumed_capacitance') + consumed_capacitance;
-
-				rels = sim.as_get_related_entities(strcat(sim.sid, '.', fn_eid{i}));
-				fn_src_full_id = fieldnames(rels);
-				l = struct;
-				for j = 1:numel(fn_src_full_id)
-					if ~strcmp(rels.(fn_src_full_id{j}).type, 'Graph')
-						l.(fn_src_full_id{j}) = struct('consumed_capacitance', consumed_capacitance);
-					end			
-				end
-				sid = strrep(sim.sid, '-', '_0x2D_'); % '_0x2D_' is hex for '-'
-				m = struct;
-				m.(strcat(sid, '_0x2E_', fn_eid{i})) = l; % '_0x2E_' is hex for '.'; JSONLab will convert it, MATLab can not have points in struct fields.
-				sim.as_set_data(m);
-			end
-
-			time_next_step = time + sim.step_size;
-		end
-
-		function data = get_data(sim, args, ~)
-			outputs = args{1};
-
-			fn_eid = fieldnames(outputs);
-			for i = 1:numel(fn_eid)
-				for j = 1:numel(outputs.(fn_eid{i}))
-					if strcmp(outputs.(fn_eid{i}){j}, 'data_out')
-						data.(fn_eid{i}).('data_out').('consumed_capacitance') = sim.loads.(fn_eid{i}).('consumed_capacitance'); % Battery voltage U
-					end
-				end
-			end
-		end
 	end
 end
