73350bfb5096279cd58233ef692cd42e58d32ce3
Sasan Jacob Rasti <sj.rasti@gmail.com>
2015-10-28 13:23:54 +0100

    Add contents

---
+++ Example/ExampleBatteryLoadSim/Contents.m
@@ -0,0 +1,6 @@
+% EXAMPLEBATTERYLOADSIM
+%
+% Files
+%   Battery               - Battery model for MOSAIK ModelSimulator
+%   ExampleBatteryLoadSim - Advanced simulator.
+%   Load                  - Electrical load model for MOSAIK ModelSimulator.
