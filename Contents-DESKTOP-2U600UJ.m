9c201a93f89e1c4366b6f3dad960b3d8e452502d
Sasan Jacob Rasti <sj.rasti@gmail.com>
2015-10-29 20:13:28 +0100

    Added API toolbox

--- Contents-DESKTOP-2U600UJ.m
+++
@@ -1,9 +0,0 @@
-% +MOSAIKAPI
-%
-% Files
-%   Model             - Superclass for models.
-%   ModelSimulator    - Handles model creation and stepping.
-%   MosaikProxy       - Asynchronous request proxy for MOSAIK.
-%   SimSocket         - TCP socket client for MOSAIK.
-%   SimSocketDelegate - Abstract delegate class for SimSocket
-%   Simulator         - Superclass for simulators.
