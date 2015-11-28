55e8c6631b6e7d8d7e25c2d2d50c2a4777742037
Sasan Jacob Rasti <sj.rasti@gmail.com>
2015-11-28 16:29:38 +0100

    Added Controller documentation

--- +MosaikUtilities/Controller.m
+++ +MosaikUtilities/Controller.m
@@ -11,30 +11,37 @@ classdef Controller < MosaikAPI.Simulator
 	methods
 
 		function this = Controller(varargin)
-			% Constructor of the class Controller
+			% Constructor of the class Controller.
             %
             % Parameter:
             %  - varargin: Optional arguments.
             %
             % Return:
-            %  - this: Controller object
+            %  - this: Controller object.
 
             this = this@MosaikAPI.Simulator(varargin{:});
             
 		end
 
 		function ctrl_list = create(this,num,model,varargin)
-			%
-
-			if ~strcmp(model,'Controller')
-				error('Can only create controllers.');
-			end
+			% Creates a controller instance. Only one collector instance possible.
+            %
+            % Parameter:
+            %
+            %  - num: Double argument; amount of controllers.
+            %  - model: String argument; controller eid prefix.
+            %  - varargin: Optional arguments; graphical output, save path.
+            %
+            % Return:
+            %
+            %  - ctrl_list: Cell object; structs with created collector
+	        %                           information.
 
 			ctrl_list = [];
 
 			for i = this.amount:this.amount+num-1
 
-				this.eid = ['Controller','_',num2str(i)];
+				this.eid = [model,'_',num2str(i)];
 				ctrl.eid = this.eid;
 				ctrl.type = 'Controller';
 				ctrl.rel = {};
@@ -51,7 +58,14 @@ classdef Controller < MosaikAPI.Simulator
 		end
 
 		function time_next_step = step(this,time,varargin)
-			%
+			% Receives data from all given inputs.
+            %
+            % Parameter:
+            %  - time: Double argument; time of this simulation step.
+            %  - varargin: Optional arguments; input values.
+            %
+            % Return:
+            %  - time_next_step: Double object; time of next simulation step.
 
 			if ~isempty(varargin)
 				schedule = this.makeSchedule(varargin{1});
@@ -63,7 +77,14 @@ classdef Controller < MosaikAPI.Simulator
 		end
 
 		function value = getValue(this,id,attr)
-			%
+			% Receives data from all given inputs.
+            %
+            % Parameter:
+            %  - id: String argument; model id.
+            %  - attr: String arguments; requested attribute.        
+            %
+            % Return:
+            %  - value: Double object; requested value.
 
 			output.(id) = {attr,[]};
 			data = this.mosaik.get_data(output);
@@ -71,10 +92,17 @@ classdef Controller < MosaikAPI.Simulator
             
         end
 
-        function entities = getRelatedWithoutUtility(this,entities)
-        	%
+        function entities = getRelatedWithoutUtility(this,entity)
+        	% Removes utilites from related entities.
+            %
+            % Parameter:
+            %  - entity: String argument; model id.
+            %  - attr: String arguments; requested attribute.              
+            %
+            % Return:
+            %  - entities: Struct object; related entities.
 
-        	rels = this.mosaik.get_related_entities(entities);
+        	rels = this.mosaik.get_related_entities(entity);
         	rel  = fieldnames(rels);
 
         	for i = 1:numel(rel)
@@ -93,8 +121,15 @@ classdef Controller < MosaikAPI.Simulator
 
 	methods (Abstract)
 
-		% Abstract scheduling method.
-		makeSchedule(this,inputs);
+		% Creates output values for controlled models based
+		% on input values and controller function.
+        %
+        % Parameter:
+        %  - inputs: Struct argument; input values.            
+        %
+        % Return:
+        %  - schedule: Struct object; output values.
+		schedule = makeSchedule(this,inputs);
 
 	end
 
